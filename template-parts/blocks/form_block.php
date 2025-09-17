5e67dbb0624ee09c3a3130b5fe61544d10b3a3e7
Tim-DP <tim@dunepebbler.nl>
2025-09-17 15:20:06 +0200

    form block edit v3

--- template-parts/blocks/form_block.php
+++ template-parts/blocks/form_block.php
@@ -1,14 +1,19 @@
 <?php
 $form_text_field = get_sub_field('form_text_field');
 $form_form_field = get_sub_field('form_form_field');
+$form_text_img = get_sub_field('form_text_img');
 ?>
 <section class="form_block">
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-lg-1">
-            </div>
-            <div class="col-12 col-lg-3 form-text-field">
-                <?= $form_text_field; ?>
+
+            <div class="col-12 col-lg-4 form-text-field">
+                <div class="form-text-field__content">
+                    <?= $form_text_field; ?>
+                    <?php if ($form_text_img): ?>
+                        <img src="<?= $form_text_img['url']; ?>" alt="<?= $form_text_img['alt']; ?>">
+                    <?php endif; ?>
+                </div>
             </div>
             <div class="col-12 col-lg-8 form-form-field">
                 <?= $form_form_field; ?>
