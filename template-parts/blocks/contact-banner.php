8155df4f4c5cff6d6946c42c6cd9f8cbb230e5ff
Tim-DP <tim@dunepebbler.nl>
2025-09-17 14:57:47 +0200

    cursor test

--- template-parts/blocks/contact-banner.php
+++ template-parts/blocks/contact-banner.php
@@ -6,7 +6,7 @@ $contact_banner_img = get_sub_field('contact-banner-img');
     <section class="contact-banner">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-12">
+                <div class="col-12 p-0">
                     <img class="img-fluid" src="<?= $contact_banner_img['url']; ?>"
                         alt="<?= isset($contact_banner_img['alt']) ? $contact_banner_img['alt'] : ''; ?>">
                 </div>
