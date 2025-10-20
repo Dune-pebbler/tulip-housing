f49ef2f297fecffd7c2d3dc3529078e2633b283c
Tim-DP <tim@dunepebbler.nl>
2025-10-20 12:41:30 +0200

    style change

--- template-parts/blocks/text_block.php
+++ template-parts/blocks/text_block.php
@@ -7,9 +7,7 @@ $text_field_background_color = get_sub_field('text_field_background_color');
         <div class="row">
             <div class="col-10 col-md-10">
                 <div class="text_block__content-container">
-                    <div class="text_block__content-container">
-                        <?= $text_field; ?>
-                    </div>
+                    <?= $text_field; ?>
                 </div>
             </div>
         </div>
