3ad5b3f50d009948e66eb675f0781bf03a78e8fc
Tim-DP <tim@dunepebbler.nl>
2025-10-20 11:35:44 +0200

    404 edit

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row flex-center">
             <div class="col-12 col-lg-6 flex-center">
-                <h1 style="margin: 0; padding: 15px 0;">Pagina niet gevonden</h1>
+                <h2 style="margin: 0; padding: 15px 0;">Pagina niet gevonden</h2>
                 <p>We hebben de pagina die u zocht niet kunnen vinden.</p>
                 <div class="buttons">
                     <a href="<?= site_url(); ?>" class="btn">Terug naar homepagina</a>
